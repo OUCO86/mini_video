1bdcb15bbd55ae64d6449c88a5d3f1ed1987d587
OUCO86 <529598224@qq.com>
2026-09-06 19:25:08 +0800

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -3,8 +3,11 @@ for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Stu
 call "%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo LIB=%LIB%
 
-cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo mini_video_dx12.cpp /link user32.lib kernel32.lib
+:: DX12 编译命令 (已添加 /I.\include)
+cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo /I.\include mini_video_dx12.cpp /link user32.lib kernel32.lib d3d12.lib dxgi.lib dxguid.lib
 
-cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo mini_video_vulkan.cpp /link user32.lib kernel32.lib
+:: Vulkan 编译命令 (已添加 /I.\include)
+cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo /I.\include mini_video_vulkan.cpp /link user32.lib kernel32.lib
 
-cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo mini_video_dx11.cpp /link user32.lib kernel32.lib
+:: DX11 编译命令 (已添加 /I.\include)
+cl.exe /permissive- /W3 /Gy /Zc:inline /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Oi /O2 /MT /FC /EHsc /nologo /I.\include mini_video_dx11.cpp /link user32.lib kernel32.lib d3d11.lib dxgi.lib dxguid.lib
